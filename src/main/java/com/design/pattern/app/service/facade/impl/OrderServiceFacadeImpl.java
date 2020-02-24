d1e16edc728f058b9403e13e1e061b98715ee3be
Rutmane <lasma.rutmane@accenture.com>
2020-02-24 10:06:56 +0200

    Final commit

--- src/main/java/com/design/pattern/app/service/facade/impl/OrderServiceFacadeImpl.java
+++ src/main/java/com/design/pattern/app/service/facade/impl/OrderServiceFacadeImpl.java
@@ -8,13 +8,17 @@ import com.design.pattern.app.services.PaymentService;
 public class OrderServiceFacadeImpl implements OrderServiceFacade{
     public boolean placeOrder(SushiRoll sushiRoll){
         boolean orderFulfilled=false;
-            System.out.println("Sushi roll: "+ sushiRoll.getName() + " is available.");
-            boolean paymentConfirmed= PaymentService.makePayment();
-            if(paymentConfirmed){
-                System.out.println("Payment confirmed...");
-                DeliverService.deliverSushi(sushiRoll);
-                System.out.println("Product delivering...");
-                orderFulfilled=true;
+            if(sushiRoll.getAvailable() == true) {
+                System.out.println("Sushi roll: "+ sushiRoll.getName() + " is available.");
+                boolean paymentConfirmed= PaymentService.makePayment();
+                if(paymentConfirmed){
+                    System.out.println("Payment confirmed...");
+                    DeliverService.deliverSushi(sushiRoll);
+                    System.out.println("Product delivering...");
+                    orderFulfilled=true;
+                }
+            } else {
+                System.out.println("Sushi roll: "+ sushiRoll.getName() + " isn't available. Try again later!");
             }
         return orderFulfilled;
     }
